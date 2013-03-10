3bec668d654878485f6e82250aacae2e6df9eaad
Takayuki Irokawa <t.irokawa@gmail.com>
2013-03-11 01:28:33 +0900

    update

--- 1/1-33.scm
+++ 1/1-33.scm
@@ -10,27 +10,21 @@
 
 ; prime?
 (define (prime? n)
-  (define (fast-prime? n times)
-    (define (expmod base exp m)
-      (cond ((= exp 0) 1)
-            ((even? exp)
-             (remainder (square (expmod base (/ exp 2) m)) m))
-            (else
-             (remainder (* base (expmod base (- exp 1) m)) m))))
-    (define (fermat-test n)
-      (define (try-it a)
-        (= (expmod a n n) a))
-      (try-it (+ 1 (random (- n 1)))))
-    (cond ((= times 0) true)
-          ((fermat-test n) (fast-prime? n (- times 1)))
-          (else false)))
-  (fast-prime? n 1000))
+  (define (divides? a b)
+    (= (remainder b a) 0))
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+  (define (smallest-divisor n)
+    (find-divisor n 2))
+  (= n (smallest-divisor n)))
   
 ; a
-(define (sum-squares-prime a b)
+(define (sum-of-prime-squares a b)
   (filtered-accumulate + 0 square a inc b prime?))
 
 ; b
-(define (product-coprime a b)
+(define (product-of-coprime-numbers a b)
   (define (coprime? n) (= (gcd n b) 1))
   (filtered-accumulate * 1 identity a inc b coprime?))
