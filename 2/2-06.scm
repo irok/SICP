6ba921078cf39ce48ddb079ce31b88f4436c46f3
Takayuki Irokawa <t.irokawa@gmail.com>
2013-04-15 20:16:23 +0900

    update 2-06

--- 2/2-06.scm
+++ 2/2-06.scm
@@ -1,4 +1,12 @@
 (define zero (lambda (f) (lambda (x) x)))
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
 
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define (num-value num)
+  ((num (lambda (n) (+ n 1))) 0))
+
+(define (plus a b)
+  (lambda (f) (lambda (x) ((b f) ((a f) x)))))
