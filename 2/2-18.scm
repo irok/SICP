8ed38d1ab65fd237af1de9104176ba5f6db50e40
Takayuki Irokawa <t.irokawa@gmail.com>
2013-04-17 20:41:40 +0900

    update

--- 2/2-18.scm
+++ 2/2-18.scm
@@ -4,3 +4,9 @@
         new
         (iter (cons (car old) new) (cdr old))))
   (iter null items))
+
+(define (my-reverse2 items)
+  (let iter((new null) (old items))
+    (if (null? old)
+        new
+        (iter (cons (car old) new) (cdr old)))))
