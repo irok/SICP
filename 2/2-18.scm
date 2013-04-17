eaeaadcf43ed27c050f1a5d09e7100a78a9829e3
Takayuki Irokawa <t.irokawa@gmail.com>
2013-04-17 20:51:03 +0900

    update

--- 2/2-18.scm
+++ 2/2-18.scm
@@ -1,12 +1,14 @@
+(define nil '())
+
 (define (my-reverse items)
   (define (iter new old)
     (if (null? old)
         new
         (iter (cons (car old) new) (cdr old))))
-  (iter null items))
+  (iter nil items))
 
 (define (my-reverse2 items)
-  (let iter((new null) (old items))
+  (let iter((new nil) (old items))
     (if (null? old)
         new
         (iter (cons (car old) new) (cdr old)))))
