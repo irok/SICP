c3fa9529dd9f6071df32d2a8d9c9b5c4dd6e6bee
Takayuki Irokawa <t.irokawa@gmail.com>
2013-07-02 10:48:18 +0900

    add 2-38 & 2-39

---
+++ 2/2-38.scm
@@ -0,0 +1,34 @@
+(define nil '())
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define fold-right accumulate)
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+
+(fold-right / 1 (list 1 2 3))
+;  1
+; 1-
+;  2
+
+(fold-left / 1 (list 1 2 3))
+; 1
+; -
+; 6
+
+(fold-right list nil (list 1 2 3))
+; (1 (2 (3 ())))
+
+(fold-left list nil (list 1 2 3))
+; (((() 1) 2) 3)
+
